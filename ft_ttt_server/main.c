0f670827cde5b50e06d7955b15510000f61c6ece
alee <alee@aleeui-MacBookPro.local>
2022-08-23 21:06:24 +0900

    feat: input ip, port

--- ft_ttt_server/main.c
+++ ft_ttt_server/main.c
@@ -6,15 +6,29 @@
 /*   By: alee <alee@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 20:48:47 by alee              #+#    #+#             */
-/*   Updated: 2022/08/23 20:48:47 by alee             ###   ########.fr       */
+/*   Updated: 2022/08/23 21:05:37 by alee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+typedef int	SOCKET;
+
+int	main(void)
 {
+	char			s_ip[128];
+	unsigned short	s_port;
+
+	printf("input IP (-1 : 127.0.0.1) : ");
+	scanf("%s", s_ip);
+	if (strcmp(s_ip, "-1") == 0)
+		strlcpy(s_ip, "127.0.0.1", sizeof(s_ip));
+	printf("input Port : ");
+	scanf("%d", &s_port);
+
+	printf("ip : %s, port : %d \n", s_ip, s_port);
 
 
-    return (0);
+	return (0);
 }
