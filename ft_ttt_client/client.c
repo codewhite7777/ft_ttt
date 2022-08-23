e468fc19ee1c71b770bc2e0b4916c8e82b2826d8
Refi Go <64242999+refigo@users.noreply.github.com>
2022-08-23 21:37:58 +0900

    feat: socket() and connect() in client

--- ft_ttt_client/client.c
+++ ft_ttt_client/client.c
@@ -18,35 +18,42 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-int	main(void) {
-	int	sock;
+typedef int SOCKET;
 
-	// socket()
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+typedef struct s_server {
+	char			ip[128];
+	unsigned short	port;
 
-	// connect()
 	struct sockaddr_in	c_addr_in;
+}				t_server;
+
+int	main(void) {
+	t_server	s_info;
 
 	// get server ip and port
-	char	s_ip[128];
-	unsigned short	s_port;
 	printf("Input server IP (-1 : 127.0.0.1): ");
-	scanf("%s", s_ip);
-	if (strcmp(s_ip, "-1") == 0)
-		strlcpy(s_ip, "127.0.0.1", sizeof(s_ip));
+	scanf("%s", s_info.ip);
+	if (strcmp(s_info.ip, "-1") == 0)
+		strlcpy(s_info.ip, "127.0.0.1", sizeof(s_info.ip));
 	printf("Input server port: ");
-	scanf("%d", &s_port);
+	scanf("%d", &(s_info.port));
+	printf("ip: %s, port: %d\n", s_info.ip, s_info.port); // test ip and port
 
-	printf("ip: %s, port: %d\n", s_ip, s_port); // test ip and port
+	// socket()
+	SOCKET	c_sock;
+	c_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	/*
-	memset(&c_addr_in, 0x00, sizeof(c_addr_in));
-	c_addr_in.sin_family = AF_INET;
-	c_addr_in.sin_port = htons(s_port);
-	inet_pton(AF_INET, s_ip, (void *)&c_addr_in.sin_addr);
-	*/
+	// connect()
+	memset(&(s_info.c_addr_in), 0x00, sizeof(s_info.c_addr_in));
+	s_info.c_addr_in.sin_family = AF_INET;
+	s_info.c_addr_in.sin_port = htons(s_info.port);
+	inet_pton(AF_INET, s_info.ip, (void *)&(s_info.c_addr_in.sin_addr));
 
+	int	connect_result = connect(c_sock, (const struct sockaddr *)(&(s_info.c_addr_in)), sizeof(s_info.c_addr_in));
+	printf("connect_result: [%d]\n", connect_result);
 	// send()
+
+
 	// recv()
 	// close()
 	return (0);
