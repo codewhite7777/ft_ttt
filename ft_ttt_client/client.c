4626694e6fc679e6f802e6de5923e01ad5e36102
Refi Go <64242999+refigo@users.noreply.github.com>
2022-08-23 21:02:13 +0900

    feat: init ft_ttt client

---
+++ ft_ttt_client/client.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 20:48:44 by mgo               #+#    #+#             */
+/*   Updated: 2022/08/23 20:48:45 by mgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int	main(void) {
+	int	sock;
+
+	// socket()
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	// connect()
+	struct sockaddr_in	c_addr_in;
+
+	// get server ip
+	char				*server_ip;
+
+	memset(&c_addr_in, 0x00, sizeof(c_addr_in));
+	c_addr_in.sin_family = AF_INET;
+	c_addr_in.sin_port = htons(s_port);
+	inet_pton(AF_INET, s_ip, (void *)&c_addr_in.sin_addr);
+
+	// send()
+	// recv()
+	// close()
+	return (0);
+}
