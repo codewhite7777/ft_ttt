f75c1edcff7a6fefdf3937faf7659eaa757dd686
alee <alee@aleeui-MacBookPro.local>
2022-08-23 21:23:47 +0900

    feat: socket()

--- ft_ttt_server/main.c
+++ ft_ttt_server/main.c
@@ -6,29 +6,40 @@
 /*   By: alee <alee@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 20:48:47 by alee              #+#    #+#             */
-/*   Updated: 2022/08/23 21:05:37 by alee             ###   ########.fr       */
+/*   Updated: 2022/08/23 21:23:18 by alee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 typedef int	SOCKET;
 
-int	main(void)
+typedef struct s_server
 {
 	char			s_ip[128];
 	unsigned short	s_port;
+	SOCKET			s_listen_sock;		
+}		t_server;
 
+
+int	main(void)
+{
+	t_server	server;
 	printf("input IP (-1 : 127.0.0.1) : ");
-	scanf("%s", s_ip);
-	if (strcmp(s_ip, "-1") == 0)
-		strlcpy(s_ip, "127.0.0.1", sizeof(s_ip));
+	scanf("%s", server.s_ip);
+	if (strcmp(server.s_ip, "-1") == 0)
+		strlcpy(server.s_ip, "127.0.0.1", sizeof(server.s_ip));
 	printf("input Port : ");
-	scanf("%d", &s_port);
+	scanf("%d", &server.s_port);
 
-	printf("ip : %s, port : %d \n", s_ip, s_port);
-
-
+	server.s_listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server.s_listen_sock == -1)
+	{
+		printf("Err : socket(...) \n");
+		return (1);
+	}
 	return (0);
 }
